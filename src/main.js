24a5166eee7c918a98a192760b00a45d0380a3bf
ryutetsu shinjo <>
2020-01-30 14:45:28 +0900

    no message

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import VueAnalytics from './plugins/vue-analytics';
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-157370117-1'
+})
 
 new Vue({
   vuetify,
--- src/plugins/vue-analytics.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import VueAnalytics from 'vue-analytics'
-
-// Configuration VueAnalytics
-Vue.use(VueAnalytics, {
-  id: 'UA-157370117-1'
-})

a44d58959e7c5632005edfc61972224c86cd9a8a
ryutetsu shinjo <>
2020-01-30 12:30:27 +0900

    add vue-analytics

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueAnalytics from './plugins/vue-analytics';
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  VueAnalytics,
   render: h => h(App)
 }).$mount('#app')
 
---
+++ src/plugins/vue-analytics.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+  id: 'UA-157370117-1',
+  autoTracking: {
+    screenview: true
+  }
+})
